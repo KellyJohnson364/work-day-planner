b37a277e816adcf97d8393b8b42af8c02759dd9a
KJ <kj3641402@gmail.com>
2021-03-22 05:26:20 -0400

    made mobile-responsive

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,18 @@
 let currentDayEl = $('#currentDay');
 let containerEl = $('.container')
 
-
+$(window).scroll(function() {    // this will work when your window scrolled.
+  var height = $(window).scrollTop();  //getting the scrolling height of window
+  if(height  > 100) {
+    $("#currentDay").css({"position": "fixed"});
+  } else{
+    $("#currentDay").css({"position": "relative"});
+  }
+});
 
 // builds the schedule within the container div
 
-for (let i=8; i<18; i++) {
+for (let i=0; i<25; i++) {
 
 let time=moment().startOf('day').add( i, "H")
 let timeSlot=moment(time).format("hA")
@@ -13,11 +20,11 @@ let timeSlot=moment(time).format("hA")
 let rowEl= $('<div class="row"></div>').appendTo(containerEl);
 let hourEl =$('<div class="hour  col-1">' + timeSlot + '</div>')
 let slotEl= $('<input type="text" class="col-9"/>')
-let saveEl= $('<div type="submit" id="'+ i +'"class=" col-1 saveBtn"></div>')
-let iconEl=$('<i class="glyphicon glyphicon-floppy-disk"></i>')
+let saveEl= $('<button type="submit" id="'+ i +'" class="glyphicon glyphicon-floppy-disk col-1 saveBtn"></button>')
+
 
 rowEl.append(hourEl, slotEl, saveEl);
-saveEl.append(iconEl)
+
 
 // assigns classes to the slots based on time
 
